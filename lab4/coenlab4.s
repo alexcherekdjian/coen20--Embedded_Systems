99506caf4b4e0e0b41f3f713b0cbb048f59d2903
alexcherekdjian <34289936+alexcherekdjian@users.noreply.github.com>
2017-12-18 12:36:40 -0800

    Update coenlab4.s

--- lab4/coenlab4.s
+++ lab4/coenlab4.s
@@ -1,34 +1,34 @@
 // Coen lab #4
 
-            .syntax     unified
-            .cpu        cortex-m4
-            .text
-            .thumb_func
-            .align      2
+        .syntax     unified
+        .cpu        cortex-m4
+        .text
+        .thumb_func
+        .align      2
 
-            .global    Discriminant
-            .global    Root1
-            .global    Root2
+        .global    Discriminant
+        .global    Root1
+        .global    Root2
 
 Discriminant:
-			MUL 	   R1,R1,R1 // b*b
-			LSL        R0,R0,2 // a*4
-			MLS		   R0,R0,R2,R1 // b^2 - 4*a*c
-			BX		   LR
+	MUL 	   R1,R1,R1 	// b*b
+	LSL        R0,R0,2 	// a*4
+	MLS	   R0,R0,R2,R1 	// b^2 - 4*a*c
+	BX	   LR
 
 Root1:
-            LDR        R3,=0
-			SUB        R1,R3,R1 // 0-b = -b
-			ADD        R1,R1,R2 // -b + sqrt_d
-			LSL		   R0,R0,1 // 2*a
-			SDIV 	   R0,R1,R0 // -b + sqrt_d / 2*a
-			BX		   LR
+        LDR        R3,=0
+	SUB        R1,R3,R1 	// 0-b = -b
+	ADD        R1,R1,R2 	// -b + sqrt_d
+	LSL	   R0,R0,1 	// 2*a
+	SDIV 	   R0,R1,R0 	// -b + sqrt_d / 2*a
+	BX	   LR
 
 Root2:
-            LDR        R3,=0
-			SUB        R1,R3,R1 // 0-b = -b
-			SUB        R1,R1,R2 // -b - sqrt_d
-			LSL		   R0,R0,1 // 2*a
-			SDIV 	   R0,R1,R0 // -b - sqrt_d / 2*a
-			BX		   LR
-
+        LDR        R3,=0
+	SUB        R1,R3,R1 	// 0-b = -b
+	SUB        R1,R1,R2 	// -b - sqrt_d
+	LSL        R0,R0,1 	// 2*a
+	SDIV 	   R0,R1,R0 	// -b - sqrt_d / 2*a
+	BX	   LR
+	.end
