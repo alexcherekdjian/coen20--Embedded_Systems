392362db9f2fa31aefb04d75cfe6cba0f35c2712
alexcherekdjian <34289936+alexcherekdjian@users.noreply.github.com>
2017-12-18 12:45:27 -0800

    Update coenlab7.s

--- lab7/coenlab7.s
+++ lab7/coenlab7.s
@@ -1,29 +1,29 @@
 // Coen lab #7
 
-            .syntax     unified
-            .cpu        cortex-m4
-            .text
-            .thumb_func
-            .align      2
+        .syntax     unified
+        .cpu        cortex-m4
+        .text
+        .thumb_func
+        .align      2
 
-            .global     UDIVby13
-            .global     SDIVby13
-            .global 	CallReturnOverhead
-            .global		MyUDIVby13
-            .global		MySDIVby13
+        .global     UDIVby13
+        .global     SDIVby13
+        .global 	CallReturnOverhead
+        .global		MyUDIVby13
+        .global		MySDIVby13
 
 CallReturnOverhead:
-			BX 			LR
+	BX 		LR
 
-SDIVby13:
-			MOV 		R1,13
-			SDIV 		R0,R0,R1
-			BX 			LR
+SDIVby13:				    // test function
+	MOV 		R1,13
+	SDIV 		R0,R0,R1
+	BX 		LR
 
-UDIVby13:
-			MOV 		R1,13
-			UDIV 		R0,R0,R1
-			BX 			LR
+UDIVby13:				    // test function
+	MOV 		R1,13
+	UDIV 		R0,R0,R1
+	BX 		LR
 
 MySDIVby13:                                 // generated code
             LDR         R1,=0x4EC4EC4F      // 2 clock cycle(s)
@@ -37,5 +37,4 @@ MyUDIVby13:                                 // generated code
             UMULL       R2,R1,R1,R0         // 2 clock cycle(s)
             LSR         R0,R1,2             // 1 clock cycle(s)
             BX          LR
-
-			.end
+	    .end
